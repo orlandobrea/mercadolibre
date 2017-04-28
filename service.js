140b4634a7ed320f368087b09e92893e189c4dc2
Orlando Brea <orlando.brea@gmail.com>
2017-04-28 15:42:20 -0300

    Service refactorizado

--- service.js
+++ service.js
@@ -1,57 +1,20 @@
+//var templateEngine = require('./miRenderEngine');
 var http = require('https');
 
-exports.search = function(q) {
-    var options = {
-      hostname: 'api.mercadolibre.com',
-      path: '/sites/MLA/search?q='+q,
-      method: 'GET',
-      headers: {'Content-Type': 'application/json'},
-      json: true
-    };
-
-    return new Promise((resolve, reject) => {
-      http.request(options, (response) => {
-        var body = '';
-        if (response.statusCode!=200) {
-          if (response.statusCode==404) {
-            reject(Error('El código de producto no existe'))
-          } else {
-            reject(Error('La operación no se pudo completar '+response.statusCode));
-          }
-        }
-        response.on('data', function(contenido) {
-          body += contenido.toString();
-        })
-
-        response.on('end', function() {
-          var listado = JSON.parse(body);
-          resolve(listado);
-        })
-      }).end();
-    })
-}
- 
-exports.getItem = function(itemCode) {
-	// Completar el código aquí
-	// El request debe ser a https://api.mercadolibre.com/items/:itemCode y luego a 
-  // https://api.mercadolibre.com/items/:itemCode/description.json
+function httpGetRequest(url) {
   var content = ''; 
-
   var options = {
     hostname: 'api.mercadolibre.com',
-    path: '/items/'+itemCode,
+    path: url,
     method: 'GET',
     json: true,
     headers: {'Content-Type': 'application/json'}
   } 
-
-
-
   return new Promise((resolve, reject) => {
       http.request(options, (response) => {
         if (response.statusCode!=200) {
           if (response.statusCode==404) {
-            reject(Error('El código de producto no existe'))
+            reject(Error('No se encontro la pagina'))
           } else {
             reject(Error('La operación no se pudo completar '+response.statusCode));
           }
@@ -60,29 +23,40 @@ exports.getItem = function(itemCode) {
           content += data;
         });
         response.on('end', () => {
-          var content2 = '';       
-          options.path = '/items/'+itemCode+'/description.json';
-          http.request(options, (response2) => {
-            if (response2.statusCode!=200) {
-              if (response2.statusCode==404) {
-                reject(Error('El código de producto no existe'))
-              } else {
-                reject(Error('La operación no se pudo completar '+response2.statusCode));
-              }
-            }
-
-            response2.on('data', (data2) => {
-              content2 += data2;
-            });
-
-            response2.on('end', () => {
-              var response = JSON.parse(content);
-              response.description = JSON.parse(content2); // Agrego en text la descripcion del producto
-              resolve(response);
-            });
-          }).end();
+          resolve(JSON.parse(content));
         });
 
     }).end();
   });
+
 }
+
+
+module.exports.search = function(q) {
+  return httpGetRequest('/sites/MLA/search?q='+q);
+}
+
+ 
+function getItem(itemCode) {
+  return httpGetRequest('/items/'+itemCode);
+}
+
+function getItemDescription(itemCode) {
+  return httpGetRequest('/items/'+itemCode+'/description');
+}
+
+module.exports.getItem = function(itemId) {
+  return new Promise((resolve, reject) => {
+    Promise.all([
+        getItem(itemId),
+        getItemDescription(itemId)
+      ]).then(content => {
+        var objItem = content[0];
+        objItem.text = content[1].text;
+        resolve(objItem);
+      }).catch(e => {
+        reject(e);
+      })
+  });
+}
+
